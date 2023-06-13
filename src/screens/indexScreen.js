27068e73524aacf59b49bd8680701eda0eb0db90
Daniel Li <zaoster777@gmail.com>
2023-06-13 11:51:27 -0500

    use value from context in indexScreen with React.useContext

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  // React has a hook called useContext which lets us get a Context's value prop
+  const value = useContext(BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <Text>{value}</Text>
     </View>
   );
 };
