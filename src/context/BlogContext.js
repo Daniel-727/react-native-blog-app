e01690e7cb88184746d195548910b1cb35315c83
Daniel Li <zaoster777@gmail.com>
2023-06-15 14:50:13 -0500

    switch to useReducer to add blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  // State === { [array of blogposts] }
+  // Action === { type: 'add'|| 'delete' || 'edit', payload: blogpost}
+
+  switch (action.type) {
+    case "add_blogPost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = () => {
+    dispatch({ type: "add_blogPost" });
+  };
+
+  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,15 +1,24 @@
 import React, { useContext } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList, Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
   // React has a hook called useContext which lets us get a Context's value prop
-  const value = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
 
   return (
     <View>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(data) => data.title}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
